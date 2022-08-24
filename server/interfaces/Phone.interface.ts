e194609616adaa4136e57dfd40af9c1db5ba17b8
EmilAvramov <e_avramov@zoho.eu>
2022-08-24 14:13:47 +0300

    created phone model and interface

---
+++ server/interfaces/Phone.interface.ts
@@ -0,0 +1,43 @@
+import {
+    CreationOptional,
+	InferAttributes,
+	InferCreationAttributes,
+	Model,
+} from 'sequelize/types';
+
+export interface PhoneModel
+	extends Model<
+		InferAttributes<PhoneModel>,
+		InferCreationAttributes<PhoneModel>
+	> {
+        id: CreationOptional<number>,
+        key: string,
+        deviceName: string,
+        deviceImage: string,
+        connectivity: string,
+        launchDate: string,
+        dimensions: string,
+        weight: string,
+        build: string,
+        sim: string,
+        display: string,
+        size: string,
+        resolution: string,
+        protection: string,
+        os: string,
+        chipset: string,
+        cpu: string,
+        gpu: string,
+        cardSlot: string,
+        internalStorage: string,
+        cameraMain: string,
+        videoMain: string,
+        cameraSelfie: string,
+        videoSelfie: string,
+        speakers: string,
+        jack: string,
+        features: string,
+        batteryCharge: string,
+        batteryType: string,
+        price: string
+    }
