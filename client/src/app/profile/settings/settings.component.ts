8155ede5cdf9a633215d9c1e22b86ca158931269
EmilAvramov <e_avramov@zoho.eu>
2022-09-12 10:04:50 +0300

    added infrastructure to profile settings component

--- client/src/app/profile/settings/settings.component.ts
+++ client/src/app/profile/settings/settings.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnDestroy } from '@angular/core';
-import { Subject } from 'rxjs';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Observable, Subject } from 'rxjs';
+import {
+	emailPattern,
+	passwordPattern,
+	phonePattern,
+} from 'src/app/shared/variables/validationPatterns';
+import { UserFacade } from 'src/app/state/user/user.facade';
+import { User } from 'src/app/state/user/user.state';
 
 @Component({
 	selector: 'app-settings',
@@ -9,7 +17,116 @@ import { Subject } from 'rxjs';
 export class SettingsComponent implements OnDestroy {
 	public completer$: Subject<void> = new Subject<void>();
 
-	constructor() {
+	public userData$: Observable<User | null> | undefined;
+
+	constructor(private fb: FormBuilder, private userFacade: UserFacade) {
+		this.userData$ = this.userFacade.userData$;
+	}
+
+	profileForm = this.fb.group({
+		email: [
+			'',
+			{
+				validators: [Validators.required, Validators.pattern(emailPattern)],
+				updateOn: 'blur',
+			},
+		],
+		firstName: [
+			'',
+			{
+				validators: [Validators.required],
+				updateOn: 'blur',
+			},
+		],
+		lastName: [
+			'',
+			{
+				validators: [Validators.required],
+				updateOn: 'blur',
+			},
+		],
+		phone: [
+			'',
+			{
+				validators: [Validators.required, Validators.pattern(phonePattern)],
+				updateOn: 'blur',
+			},
+		],
+		address: [
+			'',
+			{
+				validators: [Validators.required],
+				updateOn: 'blur',
+			},
+		],
+		city: [
+			'',
+			{
+				validators: [Validators.required],
+				updateOn: 'change',
+			},
+		],
+	});
+
+	passwordForm = this.fb.group({
+		currentPassword: [
+			'',
+			{
+				validators: [Validators.required, Validators.pattern(passwordPattern)],
+				updateOn: 'blur',
+			},
+		],
+		newPassword: [
+			'',
+			{
+				validators: [Validators.required, Validators.pattern(passwordPattern)],
+				updateOn: 'blur',
+			},
+		],
+		newPasswordRe: [
+			'',
+			{
+				validators: [Validators.required, Validators.pattern(passwordPattern)],
+				updateOn: 'change',
+			},
+		],
+	});
+
+	get email() {
+		return this.profileForm.get('email');
+	}
+	get firstName() {
+		return this.profileForm.get('firstName');
+	}
+	get lastName() {
+		return this.profileForm.get('lastName');
+	}
+	get phone() {
+		return this.profileForm.get('phone');
+	}
+	get address() {
+		return this.profileForm.get('address');
+	}
+	get city() {
+		return this.profileForm.get('city');
+	}
+	get currentPassword() {
+		return this.passwordForm.get('currentPassword');
+	}
+	get newPassword() {
+		return this.passwordForm.get('newPassword');
+	}
+	get newPasswordRe() {
+		return this.passwordForm.get('newPasswordRe');
+	}
+
+	changeDetails() {
+		const { email, firstName, lastName, phone, address, city } =
+			this.profileForm.value;
+	}
+
+	changePassword() {
+		const { newPassword } = this.passwordForm.value;
 	}
 
 	ngOnDestroy(): void {
