0cf7648ea6eed14be04012a457cac0f5a8fad4d1
EmilAvramov <e_avramov@zoho.eu>
2022-09-17 19:19:55 +0300

    adjusted karma conf and added login tests

--- src/app/auth/login/login.component.spec.ts
+++ src/app/auth/login/login.component.spec.ts
@@ -1,5 +1,8 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormBuilder } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { DebugTracingFeature } from '@angular/router';
 import { provideMockStore } from '@ngrx/store/testing';
 import { map } from 'rxjs';
 
@@ -8,11 +11,13 @@ import { LoginComponent } from './login.component';
 describe('LoginComponent', () => {
 	let component: LoginComponent;
 	let fixture: ComponentFixture<LoginComponent>;
+	let de: DebugElement;
+	let el: HTMLElement;
 
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
 			declarations: [LoginComponent],
-      providers: [provideMockStore({}), FormBuilder],
+			providers: [provideMockStore({}), FormBuilder],
 		}).compileComponents();
 
 		fixture = TestBed.createComponent(LoginComponent);
@@ -33,4 +38,43 @@ describe('LoginComponent', () => {
 		expect(email).toBe('');
 		expect(password).toBe('');
 	});
+	it('should be invalid if empty', () => {
+		component.profileForm.controls.email.setValue('');
+		component.profileForm.controls.password.setValue('');
+		expect(component.profileForm.valid).toBeFalsy();
+	});
+	it('(email) should be valid if validation passed', () => {
+		const email = component.profileForm.controls.email;
+		email.setValue('123@123.com');
+		expect(email.valid).toBeTrue();
+	});
+	it('(email) should be invalid if validation failed', () => {
+		const email = component.profileForm.controls.email;
+		email.setValue('123@12');
+		expect(email.valid).toBeFalsy();
+		email.setValue('12345');
+		expect(email.valid).toBeFalsy();
+		email.setValue('123@dot.com.');
+		expect(email.valid).toBeFalsy();
+	});
+	it('(password) should be valid if validation passed', () => {
+		const password = component.profileForm.controls.password;
+		password.setValue('abcd123!@');
+		expect(password.valid).toBeTrue();
+	});
+	it('(password) should be invalid if validation failed', () => {
+		const password = component.profileForm.controls.password;
+		password.setValue('aaaaaaaaa');
+		expect(password.valid).toBeFalsy();
+		password.setValue('12345');
+		expect(password.valid).toBeFalsy();
+		password.setValue('12345asd');
+		expect(password.valid).toBeFalsy();
+		password.setValue('a!@1');
+		expect(password.valid).toBeFalsy();
+	});
+	it('should be invalid if validation not passed', () => {});
+	it('should be valid if validation passed', () => {});
+	it('should be not be clickable if form invalid', () => {});
+	it('should be clickable if form valid', () => {});
 });
