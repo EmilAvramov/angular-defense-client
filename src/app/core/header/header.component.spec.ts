c6ee9f56db52321d97d4885ab2f91296dc07533e
EmilAvramov <e_avramov@zoho.eu>
2022-09-18 18:12:05 +0300

    added tests for header component, fixes to html

--- src/app/core/header/header.component.spec.ts
+++ src/app/core/header/header.component.spec.ts
@@ -1,4 +1,6 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { provideMockStore } from '@ngrx/store/testing';
 
 import { HeaderComponent } from './header.component';
@@ -20,5 +22,55 @@ describe('HeaderComponent', () => {
 
 	it('should create', () => {
 		expect(component).toBeTruthy();
+	});
+	it('should show guest view if no token', () => {
+		component.token = '';
+		fixture.detectChanges();
+		const loginNav: HTMLElement = fixture.debugElement.query(
+			By.css('.header__login_nav')
+		).nativeElement;
+		const registerNav: HTMLElement = fixture.debugElement.query(
+			By.css('.header__register_nav')
+		).nativeElement;
+
+		expect(loginNav).toBeTruthy();
+		expect(registerNav).toBeTruthy();
+	});
+	it('should show user view if token', () => {
+		component.token = '12312312312asdasdasd';
+		fixture.detectChanges();
+		const profileNav: HTMLElement = fixture.debugElement.query(
+			By.css('.header__dropdown')
+		).nativeElement;
+		const logoutNav: HTMLElement = fixture.debugElement.query(
+			By.css('.header__logout_nav')
+		).nativeElement;
+
+		expect(profileNav).toBeTruthy();
+		expect(logoutNav).toBeTruthy();
+	});
+	it('should show dropdown on hover', () => {
+		component.token = '12312312312asdasdasd';
+		fixture.detectChanges();
+		const dropdown: DebugElement = fixture.debugElement.query(
+			By.css('.header__dropdown')
+		);
+
+		spyOn(component, 'openDropDown');
+		dropdown.triggerEventHandler('mouseover');
+		fixture.detectChanges();
+		expect(component.openDropDown).toHaveBeenCalled();
+	});
+	it('should hide dropdown on mouse leave', () => {
+		component.token = '12312312312asdasdasd';
+		fixture.detectChanges();
+		const dropdown: DebugElement = fixture.debugElement.query(
+			By.css('.header__dropdown')
+		);
+
+		spyOn(component, 'closeDropDown');
+		dropdown.triggerEventHandler('mouseleave');
+		fixture.detectChanges();
+		expect(component.closeDropDown).toHaveBeenCalled();
 	});
 });
