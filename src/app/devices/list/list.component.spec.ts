66d015eadb58cefcd906930a27baff259447112e
EmilAvramov <e_avramov@zoho.eu>
2022-09-19 17:03:24 +0300

    added unit tests for devices list c. + mock data

--- src/app/devices/list/list.component.spec.ts
+++ src/app/devices/list/list.component.spec.ts
@@ -1,25 +1,50 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import {
+	deviceMockDataPartial,
+	deviceMockDataFull,
+} from 'src/app/shared/mockData/devices.mock';
 
 import { ListComponent } from './list.component';
 
 describe('ListComponent', () => {
-  let component: ListComponent;
-  let fixture: ComponentFixture<ListComponent>;
+	let component: ListComponent;
+	let fixture: ComponentFixture<ListComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ListComponent ],
-      schemas: [NO_ERRORS_SCHEMA]
-    })
-    .compileComponents();
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [ListComponent],
+			schemas: [NO_ERRORS_SCHEMA],
+		}).compileComponents();
 
-    fixture = TestBed.createComponent(ListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		fixture = TestBed.createComponent(ListComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+	it('should trigger load more emitter', () => {
+		component.data = deviceMockDataFull;
+		expect(component.data.length).toBeGreaterThan(18);
+		fixture.detectChanges();
+		const button: HTMLButtonElement = fixture.debugElement.query(
+			By.css('.devices__more')
+		).nativeElement;
+
+		spyOn(component, 'loadMore');
+		button.click();
+		fixture.detectChanges();
+		expect(component.loadMore).toHaveBeenCalled();
+	});
+	it('should not be visible if not enough data', () => {
+		component.data = deviceMockDataPartial;
+		expect(component.data.length).toBe(1);
+		fixture.detectChanges();
+
+		expect(fixture.debugElement.query(By.css('.devices__more'))).toBeNull();
+	});
+  it('should trigger')
 });
