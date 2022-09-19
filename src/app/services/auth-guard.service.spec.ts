7fbd92a12a7794b1ed369e2050f42b6b00ea27e3
EmilAvramov <e_avramov@zoho.eu>
2022-09-19 16:42:20 +0300

    added missing testing specs and dependencies

--- src/app/services/auth-guard.service.spec.ts
+++ src/app/services/auth-guard.service.spec.ts
@@ -1,16 +1,19 @@
 import { TestBed } from '@angular/core/testing';
+import { provideMockStore } from '@ngrx/store/testing';
 
 import { AuthGuardService } from './auth-guard.service';
 
 describe('AuthGuardService', () => {
-  let service: AuthGuardService;
+	let service: AuthGuardService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AuthGuardService);
-  });
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			providers: [provideMockStore({})],
+		});
+		service = TestBed.inject(AuthGuardService);
+	});
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+	it('should be created', () => {
+		expect(service).toBeTruthy();
+	});
 });
