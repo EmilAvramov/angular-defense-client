688dae7627759315d5beab5d4b945d71d29a43d3
EmilAvramov <e_avramov@zoho.eu>
2022-09-20 22:51:56 +0300

    finished search c. tests, ++ some create c. tests

--- src/app/marketplace/create/create.component.spec.ts
+++ src/app/marketplace/create/create.component.spec.ts
@@ -1,6 +1,13 @@
-import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
+import {
+	ComponentFixture,
+	fakeAsync,
+	TestBed,
+	tick,
+} from '@angular/core/testing';
 import { FormBuilder } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { of } from 'rxjs';
 
 import { CreateComponent } from './create.component';
 
@@ -23,6 +30,22 @@ describe('CreateComponent', () => {
 	it('should create', () => {
 		expect(component).toBeTruthy();
 	});
+	it('should emit a search string', fakeAsync(() => {
+		spyOn(component.requestDeviceList, 'emit');
+		fixture.detectChanges();
+
+		const searchInputDe: DebugElement = fixture.debugElement.query(
+			By.css('.search__input')
+		);
+		const searchInputHTML: HTMLInputElement = searchInputDe.nativeElement;
+
+		searchInputHTML.value = 'test input';
+		searchInputHTML.dispatchEvent(new Event('input'));
+		tick(1000);
+
+		fixture.detectChanges();
+		expect(component.requestDeviceList.emit).toHaveBeenCalledWith('test input');
+	}));
 	it('should close observables on component destroy', () => {
 		const next = spyOn(component.completer$, 'next');
 		const complete = spyOn(component.completer$, 'complete');
@@ -32,5 +55,51 @@ describe('CreateComponent', () => {
 
 		expect(next).toHaveBeenCalled();
 		expect(complete).toHaveBeenCalled();
+	});
+	it('should close modal on button click', () => {
+		spyOn(component, 'close').and.callThrough();
+		spyOn(component.clearDetails, 'emit');
+		spyOn(component.postingForm, 'reset');
+		component.display$ = of(true);
+		fixture.detectChanges();
+
+		const closeDebugEl: DebugElement = fixture.debugElement.query(
+			By.css('.close')
+		);
+		const closeButton: HTMLButtonElement = closeDebugEl.nativeElement;
+		const searchInputDe: DebugElement = fixture.debugElement.query(
+			By.css('.search__input')
+		);
+		const searchInputHTML: HTMLInputElement = searchInputDe.nativeElement;
+		fixture.detectChanges();
+
+		closeButton.click();
+		expect(component.close).toHaveBeenCalled();
+		expect(searchInputHTML.value).toEqual('');
+		expect(component.clearDetails.emit).toHaveBeenCalled();
+		expect(component.postingForm.reset).toHaveBeenCalled();
+	});
+	it('should close modal on wrapper click', () => {
+		spyOn(component, 'close').and.callThrough();
+		spyOn(component.clearDetails, 'emit');
+		spyOn(component.postingForm, 'reset');
+		component.display$ = of(true);
+		fixture.detectChanges();
+
+		const wrapperDE: DebugElement = fixture.debugElement.query(
+			By.css('.create__wrapper')
+		);
+		const wrapperHTML: HTMLElement = wrapperDE.nativeElement;
+		const searchInputDe: DebugElement = fixture.debugElement.query(
+			By.css('.search__input')
+		);
+		const searchInputHTML: HTMLInputElement = searchInputDe.nativeElement;
+		fixture.detectChanges();
+
+		wrapperHTML.click();
+		expect(component.close).toHaveBeenCalled();
+		expect(searchInputHTML.value).toEqual('');
+		expect(component.clearDetails.emit).toHaveBeenCalled();
+		expect(component.postingForm.reset).toHaveBeenCalled();
 	});
 });
--- src/app/marketplace/search/search.component.spec.ts
+++ src/app/marketplace/search/search.component.spec.ts
@@ -1,5 +1,13 @@
-import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
+import {
+	ComponentFixture,
+	fakeAsync,
+	TestBed,
+	tick,
+} from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { of } from 'rxjs';
+import { mockUser } from 'src/app/shared/mockData/users.mock';
 
 import { SearchComponent } from './search.component';
 
@@ -21,4 +29,39 @@ describe('SearchComponent', () => {
 	it('should create', () => {
 		expect(component).toBeTruthy();
 	});
+	it('(button) should not be visible to guests', () => {
+		expect(fixture.debugElement.query(By.css('.search__create'))).toBeNull();
+	});
+	it('(button) should be visible to users', () => {
+		component.user$ = of(mockUser);
+		spyOn(component, 'onCreate').and.callThrough();
+		spyOn(component.create, 'emit');
+		fixture.detectChanges();
+
+		const buttonDE: DebugElement = fixture.debugElement.query(
+			By.css('.search__create')
+		);
+		const buttonHTML: HTMLButtonElement = buttonDE.nativeElement;
+		buttonHTML.click();
+		fixture.detectChanges();
+
+		expect(component.onCreate).toHaveBeenCalled();
+		expect(component.create.emit).toHaveBeenCalled();
+	});
+	it('should emit a search string', fakeAsync(() => {
+		spyOn(component.search, 'emit');
+		fixture.detectChanges();
+
+		const searchInputDe: DebugElement = fixture.debugElement.query(
+			By.css('.search__input')
+		);
+		const searchInputHTML: HTMLInputElement = searchInputDe.nativeElement;
+
+		searchInputHTML.value = 'test input';
+		searchInputHTML.dispatchEvent(new Event('input'));
+		tick(1000);
+
+		fixture.detectChanges();
+		expect(component.search.emit).toHaveBeenCalledWith('test input');
+	}));
 });
