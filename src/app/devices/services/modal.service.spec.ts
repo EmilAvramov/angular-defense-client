f33bb0ccffeccb6fd177dd9d3eebcf983f6c6e1f
EmilAvramov <e_avramov@zoho.eu>
2022-09-20 14:31:36 +0300

    tests for d. service + fake c. for marketplace

--- src/app/devices/services/modal.service.spec.ts
+++ src/app/devices/services/modal.service.spec.ts
@@ -12,4 +12,22 @@ describe('ModalService', () => {
 	it('should be created', () => {
 		expect(service).toBeTruthy();
 	});
+	it('should init display with false', () => {
+		let state: boolean = false;
+		service.watch().subscribe((value: boolean) => (state = value));
+		expect(state).toBeFalsy();
+	});
+	it('should advanced observable to true on open', () => {
+		let state: boolean = false;
+		service.open();
+		service.watch().subscribe((value: boolean) => (state = value));
+		expect(state).toBeTruthy();
+	});
+	it('should open and close observable in succession', () => {
+		let state: boolean = false;
+		service.open();
+		service.close();
+		service.watch().subscribe((value: boolean) => (state = value));
+		expect(state).toBeFalsy();
+	});
 });
--- src/app/marketplace/marketplace.component.spec.ts
+++ src/app/marketplace/marketplace.component.spec.ts
@@ -1,8 +1,59 @@
-import { NO_ERRORS_SCHEMA } from '@angular/core';
+import {
+	Component,
+	EventEmitter,
+	Input,
+	NO_ERRORS_SCHEMA,
+	Output,
+} from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideMockStore } from '@ngrx/store/testing';
+import { Observable } from 'rxjs';
+import { Device } from '../state/device/device.state';
+import { Posting, PostingPayload } from '../state/posting/posting.state';
+import { User } from '../state/user/user.state';
+import { CreateComponent } from './create/create.component';
+import { DetailsComponent } from './details/details.component';
+import { ListComponent } from './list/list.component';
 
 import { MarketplaceComponent } from './marketplace.component';
+import { SearchComponent } from './search/search.component';
+
+@Component({
+	selector: 'app-list',
+	template: '',
+})
+class FakeListComponent implements Partial<ListComponent> {
+	@Input() postings$: Observable<Posting[] | null> | undefined;
+	@Output() requestMore = new EventEmitter<null>();
+	@Output() requestDetails = new EventEmitter<number>();
+}
+
+class FakeCreateComponent implements Partial<CreateComponent> {
+	@Input() user$: Observable<User | null> | undefined;
+	@Input() devices$: Observable<Device[] | null> | undefined;
+	@Input() deviceDetails$: Observable<Device | null> | undefined;
+	@Output() requestDeviceList = new EventEmitter<string>();
+	@Output() requestDeviceDetails = new EventEmitter<string>();
+	@Output() createPosting = new EventEmitter<PostingPayload>();
+	@Output() clearDetails = new EventEmitter<null>();
+}
+
+class FakeDetailsComponent implements Partial<DetailsComponent> {
+	@Input() details$: Observable<Posting | null> | undefined;
+	@Input() validatedUser$: Observable<User | null> | undefined;
+	@Output() editPosting = new EventEmitter<{
+		id: number;
+		comments: string;
+		price: number;
+	}>();
+	@Output() deletePosting = new EventEmitter<number>();
+}
+
+class FakeSearchComponent implements Partial<SearchComponent> {
+	@Input() user$: Observable<User | null> | undefined;
+	@Output() search = new EventEmitter<string>();
+	@Output() create = new EventEmitter<null>();
+}
 
 describe('MarketplaceComponent', () => {
 	let component: MarketplaceComponent;
@@ -10,7 +61,13 @@ describe('MarketplaceComponent', () => {
 
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
-			declarations: [MarketplaceComponent],
+			declarations: [
+				MarketplaceComponent,
+				FakeCreateComponent,
+				FakeDetailsComponent,
+				FakeListComponent,
+				FakeSearchComponent,
+			],
 			providers: [provideMockStore({})],
 			schemas: [NO_ERRORS_SCHEMA],
 		}).compileComponents();
