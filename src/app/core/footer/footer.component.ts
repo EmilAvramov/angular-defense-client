582acaef5ac4237ec7af61d4aac8c06b27610316
EmilAvramov <e_avramov@zoho.eu>
2022-09-23 17:23:28 +0300

    greatly improved footer UX and component logic

--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-footer',
@@ -6,9 +7,39 @@ import { Component } from '@angular/core';
 	styleUrls: ['./footer.component.sass'],
 })
 export class FooterComponent {
-	constructor() {}
+	constructor(private router: Router) {}
 
 	scrollToTop() {
 		window.scrollTo({ top: 0, behavior: 'smooth' });
+	}
+
+	marketNav() {
+		let location: string = window.location.href.split('/')[3];
+		if (location === 'marketplace') {
+			this.scrollToTop();
+		} else {
+			this.router.navigate(['/marketplace']);
+			window.scroll(0, 0);
+		}
+	}
+
+	mapNav(current: string) {
+		let location: string = window.location.href.split('/')[3];
+		if (location === current) {
+			this.scrollToTop();
+		} else {
+			this.router.navigate([current]);
+			window.scroll(0, 0);
+		}
+	}
+
+	logoNavigate() {
+		let location: string = window.location.href.split('/')[3];
+		if (location) {
+			this.router.navigate(['/']);
+			window.scroll(0, 0);
+		} else {
+			this.scrollToTop();
+		}
 	}
 }
