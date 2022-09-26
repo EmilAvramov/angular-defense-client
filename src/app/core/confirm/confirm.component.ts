743c2cf879b5fc5d1e6a26eed18893841d8d0326
EmilAvramov <e_avramov@zoho.eu>
2022-09-26 18:36:17 +0300

    adjustments to confirm dialog component

--- src/app/core/confirm/confirm.component.ts
+++ src/app/core/confirm/confirm.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ModalService } from 'src/app/services/modal.service';
 
@@ -14,10 +14,29 @@ export class ConfirmComponent {
 	btnOkText!: string;
 	btnCancelText!: string;
 
+	@Output() answer = new EventEmitter<boolean>();
+
 	constructor(private modal: ModalService) {
 		this.display$ = this.modal.watch();
 	}
 	close() {
 		this.modal.close();
+	}
+
+	confirm() {
+		this.answer.emit(true);
+		this.close();
+	}
+
+	cancel() {
+		this.answer.emit(false);
+		this.close();
+	}
+
+	loadData(message: string, ok: string, cancel: string) {
+		this.message = message;
+		this.btnOkText = ok;
+		this.btnCancelText = cancel;
+		this.modal.open();
 	}
 }
