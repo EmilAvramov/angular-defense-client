4d54a34eee0800bfebfc97f22d1a0e9e1e140b4c
EmilAvramov <e_avramov@zoho.eu>
2022-09-27 21:40:40 +0300

    overhaul edit component logic in profile

--- src/app/profile/postings/edit/edit.component.ts
+++ src/app/profile/postings/edit/edit.component.ts
@@ -1,24 +1,27 @@
 import {
+	AfterViewInit,
+	ChangeDetectorRef,
 	Component,
-	ElementRef,
 	EventEmitter,
 	Input,
+	OnDestroy,
 	Output,
-	ViewChild,
 } from '@angular/core';
-import { map, Observable } from 'rxjs';
+import { map, Observable, Subject, takeUntil } from 'rxjs';
 import { ModalService } from 'src/app/services/modal.service';
 import { Posting } from 'src/app/state/posting/posting.state';
 import { UserFacade } from 'src/app/state/user/user.facade';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { ConfirmDialog } from 'src/app/core/confirm/confirm.component';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
 	selector: 'app-edit',
 	templateUrl: './edit.component.html',
 	styleUrls: ['./edit.component.sass'],
 })
-export class EditComponent {
+export class EditComponent implements AfterViewInit, OnDestroy {
+	public completer$: Subject<void> = new Subject<void>();
 	public display$!: Observable<boolean>;
 
 	@Input() details$: Observable<Posting | null> | undefined;
@@ -29,23 +32,63 @@ export class EditComponent {
 	}>();
 	@Output() deletePosting = new EventEmitter<number>();
 
-	@ViewChild('comments') comments!: ElementRef<HTMLInputElement>;
-	@ViewChild('price') price!: ElementRef<HTMLInputElement>;
-
 	constructor(
 		private editModal: ModalService,
 		public userFacade: UserFacade,
-		public dialog: MatDialog
+		public dialog: MatDialog,
+		public fb: FormBuilder,
+		private cdr: ChangeDetectorRef
 	) {
 		this.display$ = this.editModal.watch();
 	}
+	ngAfterViewInit(): void {
+		this.details$
+			?.pipe(
+				takeUntil(this.completer$),
+				map((data: Posting | null) => {
+					this.postingForm.patchValue({
+						comments: data?.comments,
+						price: String(data?.price),
+					});
+				})
+			)
+			.subscribe();
+		this.cdr.detectChanges();
+	}
+
+	get comments() {
+		return this.postingForm.get(['comments']);
+	}
+
+	get price() {
+		return this.postingForm.get(['price']);
+	}
+
+	postingForm = this.fb.group({
+		comments: [
+			'',
+			{
+				validators: [Validators.required],
+				updateOn: 'change',
+			},
+		],
+
+		price: [
+			'',
+			{
+				validators: [Validators.required],
+				updateOn: 'change',
+			},
+		],
+	});
 
 	emitEdit(id: number) {
 		this.editPosting.emit({
 			id,
-			comments: this.comments.nativeElement.value,
-			price: Number(this.price.nativeElement.value),
+			comments: this.comments?.value,
+			price: Number(this.price?.value),
 		});
+		this.editModal.close();
 	}
 
 	emitDelete(id: number) {
@@ -75,5 +118,10 @@ export class EditComponent {
 	}
 	close() {
 		this.editModal.close();
+	}
+
+	ngOnDestroy(): void {
+		this.completer$.next();
+		this.completer$.complete();
 	}
 }
